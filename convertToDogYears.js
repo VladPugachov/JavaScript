8b793a84ac54c7f64666c7d86919c6a032f9beda
VladPugachov <root@LAPTOP-99T9R048>
2023-02-05 10:21:47 +0200

    Add new file

---
+++ convertToDogYears.js
@@ -0,0 +1,10 @@
+const convertToDogYears = (myAge) => {
+    let earlyYears = 2; //First 2 dog years counting as 10.5 
+    earlyYears *= 10.5;
+    let laterYears = myAge -2;//Other dog years counting as 4
+    laterYears *= 4;
+    let myAgeInDogYears = earlyYears + laterYears;
+    return `I am ${myAge} years old in human years which is 
+    ${myAgeInDogYears} years old in dog years.`;   
+}
+console.log(convertToDogYears(40));
